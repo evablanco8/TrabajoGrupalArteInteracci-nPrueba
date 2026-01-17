e3c61afdb5d6eb0c7ee805c645249b69c1e42ada
evablanco8 <eva.blanco1@alu.uclm.es>
2026-01-17 18:52:03 +0100

    NPCs y programación casa

--- Prueba 2D/Assets/Objetos/Casa.cs
+++ Prueba 2D/Assets/Objetos/Casa.cs
@@ -1,26 +1,71 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class CartelScript : MonoBehaviour
+public class CasaScript : MonoBehaviour
 {
+    [Header("Zonas")]
+    public Collider2D zonaVision;
+    public Collider2D zonaCasa; 
+
     public bool enZona;
+    public bool tiempoEspera;
     public bool vision;
+    
+    void Start()
+    {
+        StartCoroutine(EsperarDosMinutos());
+        tiempoEspera = false;
+        vision = false;
+        enZona =false;
+    }
+    IEnumerator EsperarDosMinutos()
+    {
+        yield return new WaitForSeconds(120f); // 2 minutos
+        tiempoEspera = true;
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        // ZONA DE VISIÓN (la lejana)
+        if (collision == zonaVision)
+        {
+            vision = true;
+        }
+
+        // ZONA DE LA CASA (donde está el objeto)
+        if (collision == zonaCasa)
         {
             enZona = true;
         }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (collision == zonaVision)
+        {
+            vision = false;
+        }
+
+        if (collision == zonaCasa)
         {
             enZona = false;
         }
     }
     void Update()
     {
+        if (tiempoEspera==true && vision==true)
+        {
+           gameObject.SetActive(false);
+        }
         if (vision==false && enZona==true)
         {
             SceneManager.LoadScene("Nivel 2");
